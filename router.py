435000fc0b4e70d1420c8fdd3e07d97eee9ca1b6
Silvana Mara Ribeiro <silvana.mribeiro@yahoo.com.br>
2018-05-27 10:29:39 -0300

    add, del, json, loopback functions tested and working

--- router.py
+++ router.py
@@ -30,32 +30,36 @@ def loopback(operation):
 	 subprocess.call(['./tests/lo-adresses.sh',operation]) 
 	 
 def add_ve(ip, weight, routing_table):
-	return routing_table[ip] = weight
+	routing_table[ip] = weight
+	return routing_table
 
 def del_ve(ip, routing_table):
-	return del routing_table[ip]
+	return routing_table.pop(ip)
 	
-def read_file(file_name):
-	with open(file_name, "r") as f:
-		command = f.readline()
-		commands = command.split(" ")
-		if commands[0] !== 'add':
-			print("Inavlid command was read in startup file:", commands[0])
-		if len(commands) != 3:
-			print("Inavlid format was read in startup file:", commands)
-		else:
-			add_ve(commands[1], commands[2])
+def read_file(file_name, routing_table):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            command = line.replace('\n', '')
+            commands = command.split(" ")
+            print(commands)
+            if commands[0] != 'add':
+                print("Inavlid command was read in startup file:", commands[0])
+            if len(commands) != 3:
+                print("Inavlid format was read in startup file:", commands)
+            else:
+                add_ve(commands[1], commands[2], routing_table)
 			
 def encode_message(type, source, destination, last_info):
 	if type is 'data':
 		return json.dumps({'type': type, 'source': source, 'destination': destination, 'payload': last_info})
-	elif type is 'update':
+	elif type is 'update': # tem que arrumar o distances. Eh outro json.
 		return json.dumps({'type': type, 'source': source, 'destination': destination, 'distances': last_info})
 	elif type is 'trace':
 		return json.dumps({'type': type, 'source': source, 'destination': destination, 'hops': last_info})
 
 def decode_message(message):
-	data = json.load(message)
+	data = json.loads(message)
 	# Prints if it's data type message
 	if data["type"] is 'data':
 		pprint(data)
